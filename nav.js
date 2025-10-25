206816ff7ef9c897ea8b6b5e7ab2e3953d1f3134
Jonathan Boyle <jboyle9823@gmail.com>
2025-10-25 16:50:24 -0400

    Hopefully final debug

--- nav.js
+++ nav.js
@@ -9,18 +9,17 @@ function setNav(current_path){
     current_path = splitAtRoot(current_path);
     fetch("nav.html")
         .then(r => r.text())
-        .then(html => {
-            document.getElementById("main-nav").innerHTML = html;
-            const nav = document.getElementById("main-nav");
-            for (let child of nav.children) {
-                if (child instanceof HTMLAnchorElement) {
-                    console.log("Check One");
-                    const childPath = splitAtRoot(child.href);
-                    if (childPath == current_path) {
-                        child.classList.add("current");
-                        console.log("Check Two");
-                    }
+        .then(html => {document.getElementById("main-nav").innerHTML = html;});
+    
+    const nav = document.getElementById("main-nav");
+    for (let child of nav.children) {
+        if (child instanceof HTMLAnchorElement) {
+            console.log("Check One");
+            const childPath = splitAtRoot(child.href);
+            if (childPath === current_path) {
+                child.classList.add("current");
+                console.log("Check Two");
                 }
             }
-        });
-}
+        }
+    };
