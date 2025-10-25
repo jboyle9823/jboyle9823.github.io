527192405310db1c20769c7ee491a2beb90a8688
Jonathan Boyle <jboyle9823@gmail.com>
2025-10-25 13:24:11 -0400

    second commit for lab6 p1, for testing

--- nav.js
+++ nav.js
@@ -1,3 +1,24 @@
-function setNav(){
-    fetch("nav.html").then(r => r.text()).then(html => document.getElementById("main-nav").innerHTML = html);
+function splitAtRoot(path){
+    const url = new URL(path, location.origin);
+    const pathFromRoot = url.pathname;
+    document.write("<br>----> path from root: " + pathFromRoot);
+    return pathFromRoot;
+}
+
+function setNav(current_path){
+    current_path = splitAtRoot(current_path);
+    fetch("nav.html")
+        .then(r => r.text())
+        .then(html => {
+            document.getElementById("main-nav").innerHTML = html;
+            const nav = document.getElementById("main-nav");
+            for (let child of nav.children) {
+                if (child instanceof HTMLAnchorElement) {
+                    const childPath = splitAtRoot(child.href);
+                    if (childPath === current_path) {
+                        child.classList.add("current_page");
+                    }
+                }
+            }
+        });
 }
